7163492b7dcf4a7d1cfaa7105fd4f6210273c288
Diego Lapiduz <diego.lapiduz@gsa.gov>
2016-04-01 23:41:23 -0500

    Ignore Broadcast Requests

--- src/harden/bin/start.sh
+++ src/harden/bin/start.sh
@@ -41,8 +41,9 @@ ICMP_SETTINGS[13]="net.ipv6.conf.default.accept_source_route=0"
 ICMP_SETTINGS[14]="net.ipv4.conf.all.log_martians=1"
 ICMP_SETTINGS[15]="net.ipv4.conf.default.log_martians=1"
 ICMP_SETTINGS[16]="net.ipv4.ip_forward=0"
-ICMP_SETTINGS[17]="net.ipv4.route.flush=1"
-ICMP_SETTINGS[18]="net.ipv6.route.flush=1"
+ICMP_SETTINGS[17]="net.ipv4.icmp_echo_ignore_broadcasts=1"
+ICMP_SETTINGS[18]="net.ipv4.route.flush=1"
+ICMP_SETTINGS[19]="net.ipv6.route.flush=1"
 
 for setting in "${ICMP_SETTINGS[@]}"; do
   /sbin/sysctl -w $setting

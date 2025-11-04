ee679befbf3e9e7199191bd816e056f1b17faccf
Steve Greenberg <stephen.greenberg@gsa.gov>
2025-11-04 07:29:35 -0700

    bosh requires an integer denoting sleep time at the end of a drain

--- jobs/ubuntu-advantage-pro-token/templates/drain.sh
+++ jobs/ubuntu-advantage-pro-token/templates/drain.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+
+set -eo pipefail
+
+exec 3>&1
+exec 1>> /var/vcap/sys/log/ubuntu-advantage-pro-token/drain.stdout.log
+exec 2>> /var/vcap/sys/log/ubuntu-advantage-pro-token/drain.stderr.log
+
 echo "Detaching Pro License"
 its_ok="pro detach failed. This likely means the machine was already detached from a license token."
 pro detach --assume-yes || echo "$its_ok"
+
+echo 0 >&3
